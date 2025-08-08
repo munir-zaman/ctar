5037826e97899e5973228e41eb7ef13e6e1b47a3
Munir Zaman <munir.zaman2k22@outlook.com>
2025-08-08 23:13:51 +0600

    read_tar_entry

--- extract.c
+++ extract.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// NOTE: strings inside this sturct are not null terminated except `ustar`
+/* NOTE: strings inside this sturct are not null terminated except `ustar` */
 struct tar_header {
     char file_path[100];
     char file_mode[8];
@@ -27,9 +27,11 @@ struct tar_header {
 struct tar_entry {
     struct tar_header *header;
     long offset;
+    size_t data_chunks;
+    size_t data_size;
 };
 
-void parse_tar_header(struct tar_header *header, const char *buffer) {
+void tar_header_parse(struct tar_header *header, const char *buffer) {
     /* copy header data from buffer */
     memcpy(header->file_path,           buffer +   0,   100);
     memcpy(header->file_mode,           buffer + 100,   8);
@@ -38,7 +40,7 @@ void parse_tar_header(struct tar_header *header, const char *buffer) {
     memcpy(header->file_size,           buffer + 124,   12);
     memcpy(header->last_mod_time,       buffer + 136,   12);
     memcpy(header->checksum,            buffer + 148,   8);
-    memcpy(&header->type,                buffer + 156,   1);
+    memcpy(&header->type,               buffer + 156,   1);
     memcpy(header->linked_file_name,    buffer + 157,   100);
     memcpy(header->ustar,               buffer + 257,   6);
     memcpy(header->ustar_version,       buffer + 263,   2);
@@ -50,6 +52,40 @@ void parse_tar_header(struct tar_header *header, const char *buffer) {
     memcpy(header->padding,             buffer + 500,   12);
 }
 
+int read_tar_entry(struct tar_entry *entry, FILE *fp) {
+    /*
+     * reads one entry from the file
+     */
+    char buffer[512];
+    size_t bytes_read;
+    int fread_status = ((bytes_read = fread(buffer, 1, 512, fp)) == 512);
+    /* TODO: maybe deal with this in a better way and handle error codes better?*/
+    if (!fread_status) {
+        printf("error read_tar_entry : couldn't read 512 bytes. read %d bytes.", bytes_read);
+        return -1;
+    }
+
+    tar_header_parse(entry->header, buffer);
+    /* TODO: check for `ustar`? */
+    entry->offset = ftell(fp);
+
+    /* null terminate header.full_size */
+    char ascii_file_size[13];
+    memcpy(ascii_file_size, entry->header->file_size, 12);
+    ascii_file_size[12] = '\0';
+    
+    /* convert file size in ascii octal to decimal */
+    size_t file_size = strtol(ascii_file_size, NULL, 8);
+    size_t chunks = (file_size + 511) / 512;
+    entry->data_chunks = chunks;
+    entry->data_size = 512 * chunks;
+
+    /* we need to advance the pointer and skip all the data so that the next read starts from the next entry */
+    fseek(fp, (long)entry->data_size, SEEK_CUR);
+
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
     FILE *fp;
@@ -58,38 +94,25 @@ int main(int argc, char* argv[])
     } else {
         return 1;
     }
-
-    char buffer[512];
-    while (true) {
-        int fread_status = (fread(buffer, 1, 512, fp) == 512);
-        if (!fread_status) break;
-
+    
+    int status = 0;
+    while (status == 0 && !feof(fp)) {
         struct tar_header header;
         struct tar_entry entry;
         entry.header = &header;
-
-        parse_tar_header(&header, buffer);
-        entry.offset = ftell(fp);
-
-        // null terminate header.full_size
-        char ascii_file_size[13];
-        memcpy(ascii_file_size, header.file_size, 12);
-        ascii_file_size[12] = '\0';
+        status = read_tar_entry(&entry, fp);
         
-        // convert file size in ascii octal to decimal
-        size_t file_size = strtol(ascii_file_size, NULL, 8);
-        size_t chunks = (file_size + 511) / 512;
-        size_t total_size = 512 * chunks;
+        size_t total_size = entry.data_size;
+        size_t chunks = entry.data_chunks;
+        printf("chunks: %d total_size: %d\n", chunks, total_size);
 
-        printf("file_size: %d chunks: %d total_size: %d\n", file_size, chunks, total_size);
-
-        char *data = (char *) malloc(total_size + 1);
-        data[total_size] = '\0';
-        fread(data, 1, total_size, fp);
-
-        printf("%s\n", data);
-
-        free(data);
+//        char *data = (char *) malloc(total_size + 1);
+//        data[total_size] = '\0';
+//        fread(data, 1, total_size, fp);
+//
+//        printf("%s\n", data);
+//
+//        free(data);
     }
 
     fclose(fp);
